2ff475140e1f799c684ca8cfc8c841a009d45d0e
Alexey Kudrinsky <kudrinsky.am@gmail.com>
2020-11-24 11:04:27 +0500

    rectangles can be drawn from instrument

--- kudry/window/FigureDrawer/FigureDrawer.cpp
+++ kudry/window/FigureDrawer/FigureDrawer.cpp
@@ -27,13 +27,23 @@ void RectangleDrawer::OnRelease()
 void RectangleDrawer::OnMove(FlatObj coords)
 {
     auto& origin = managingShape.GetOrigin();
+    if (origin.x <=0.001 and origin.y <= 0.001)
+        return;
     managingShape.SetSize({coords.x - origin.x, coords.y - origin.y});
 }
 
 void RectangleDrawer::OnDrawingClick(FlatObj coords)
 {
-    LOGS("Active click")
     managingShape.SetOrigin(coords);
+    LOGS("Active click: coords are (%5.5lf, %5.5lf)", managingShape.GetOrigin().x,  managingShape.GetOrigin().y)
+}
+
+void RectangleDrawer::OnDrawingRelease(FlatObj)
+{
+    LOGS("Active release")
+    isActive = false;
+    managingShape.SetOrigin({0.0, 0.0});
+    managingShape.SetSize({0.0, 0.0});
 }
 
 void RectangleDrawer::Draw(engineInterface* Canvas)
@@ -43,12 +53,6 @@ void RectangleDrawer::Draw(engineInterface* Canvas)
     {
         Canvas->DrawRect(managingShape, col);
     }
-}
-
-void RectangleDrawer::OnDrawingRelease(FlatObj)
-{
-    LOGS("Active release")
-    isActive = false;
 }
 
 bool RectangleDrawer::HandleEvent(Event *event)
